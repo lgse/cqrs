a301c9717dbc2adac66f6924a25890868059c5d0
Pierre Berube <pierre@safebase.io>
2025-06-10 23:34:15 -0600

    test: add tests for event and query

--- test/helpers/validator.spec.ts
+++ test/helpers/validator.spec.ts
@@ -1,6 +1,6 @@
-import { IsInt, IsNotEmpty, IsString } from 'class-validator';
+import { IsIn, IsInt, IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
-import { AbstractCommand } from '../../src';
+import { AbstractCommand, AbstractEvent, AbstractQuery } from '../../src';
 
 class TestCommand extends AbstractCommand<TestCommand> {
   @IsString()
@@ -11,8 +11,21 @@ class TestCommand extends AbstractCommand<TestCommand> {
   public phoneNumber: number;
 }
 
+class TestEvent extends AbstractEvent<TestEvent> {
+  @IsUUID()
+  public id: string;
+}
+
+class TestQuery extends AbstractQuery<TestQuery> {
+  @IsIn(['asc', 'desc'])
+  public order: 'asc' | 'desc';
+
+  @IsInt()
+  public page: number;
+}
+
 describe('Validator', () => {
-  it('should validate the command', () => {
+  it('should validate a command', () => {
     expect(
       () => new TestCommand({ name: 'test', phoneNumber: 1234567890 }),
     ).not.toThrow();
@@ -21,5 +34,22 @@ describe('Validator', () => {
       // @ts-expect-error we're forcing an error
       () => new TestCommand({ name: 123, phoneNumber: '1234567890' }),
     ).toThrow('name must be a string');
+  });
+
+  it('should validate an event', () => {
+    expect(
+      () => new TestEvent({ id: '123e4567-e89b-12d3-a456-426614174000' }),
+    ).not.toThrow();
+
+    expect(() => new TestEvent({ id: 'test' })).toThrow('id must be a UUID');
+  });
+
+  it('should validate a query', () => {
+    expect(() => new TestQuery({ order: 'asc', page: 1 })).not.toThrow();
+
+    // @ts-expect-error we're forcing an error
+    expect(() => new TestQuery({ order: 'test', page: 1 })).toThrow(
+      'order must be one of the following values: asc, desc',
+    );
   });
 });
